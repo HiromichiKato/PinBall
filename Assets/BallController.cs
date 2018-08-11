e356f6c8adac4aa79b6301129617c030f346cac9
HiromichiKato <cp3xzayn@icloud.com>
2018-08-11 16:48:09 +0900

    PinBall プロジェクト初めてのコミット
    ポイントを表示するUIを作成しました。

---
+++ Assets/BallController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    GameObject pointcontroller;
+
+	// Use this for initialization
+	void Start () {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.pointcontroller = GameObject.Find("PointController");
+	}
+
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag == "LargeStarTag") {
+            this.pointcontroller.GetComponent<PointController>().TouchLargeStar();
+        } else if (other.gameObject.tag == "LargeCloudTag") {
+            this.pointcontroller.GetComponent<PointController>().TouchLargeCloud();
+        } else if (other.gameObject.tag == "SmallStarTag") {
+            this.pointcontroller.GetComponent<PointController>().TouchSmallStar();
+        } else if (other.gameObject.tag == "SmallCloudTag") {
+            this.pointcontroller.GetComponent<PointController>().TouchSmallCloud();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ) {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+	}
+}
---
+++ Assets/PointController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointController : MonoBehaviour {
+
+    GameObject pointText;
+    int point = 0;
+
+    public void TouchLargeStar() {
+        this.point += 20;
+    }
+
+    public void TouchLargeCloud() {
+        this.point += 20;
+    }
+
+    public void TouchSmallStar() {
+        this.point += 10;
+    }
+
+    public void TouchSmallCloud() {
+        this.point += 10;
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        this.pointText = GameObject.Find("PointText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.pointText.GetComponent<Text>().text = this.point.ToString() + "point";
+	}
+}
